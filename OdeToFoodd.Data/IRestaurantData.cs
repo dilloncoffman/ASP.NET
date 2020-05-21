2b5412588e2e87f44328105f6d0c69a194547279
Dillon Coffman <dcoffman@ceiamerica.com>
2020-05-21 14:59:42 -0400

    Initial commit includes a web project, a class library project for Restaurant class and a class library project for dealing with Restaurant data functionality to be returned in the web project's IndexModel file from OnGet()

---
+++ OdeToFoodd.Data/IRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using OdeToFood.Core;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Dillon's Pizza", Location = "Pittsburgh", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Suzy's", Location = "Mt. Pleasant", Cuisine = CuisineType.None},
+                new Restaurant {Id = 3, Name = "Emporio", Location = "Pittsburgh", Cuisine = CuisineType.Italian}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants 
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
