afbcce81fd9ea2544746195a1aaa6e68c22db2af
Dillon Coffman <dcoffman@ceiamerica.com>
2020-05-21 16:55:01 -0400

    Added Edit page and route for Restaurants

---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        // Use this data service to create a property the view can bind to
+        public Restaurant Restaurant { get; set; }
+
+        public EditModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound"); // redirect user to NotFound
+            }
+            return Page(); // returns normal page
+        }
+    }
+}
